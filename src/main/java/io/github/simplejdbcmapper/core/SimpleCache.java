1190534e390d4fee513fe2b1f85e91f200124de1
ajoseph <ajosephmi@yahoo.com>
2025-10-11 11:20:29 -0400

    using a final static variable

--- src/main/java/io/github/simplejdbcmapper/core/SimpleCache.java
+++ src/main/java/io/github/simplejdbcmapper/core/SimpleCache.java
@@ -4,9 +4,10 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 class SimpleCache<K, V> {
+	private static final int UNLIMITED = -1;
 	private Map<K, V> cache = new ConcurrentHashMap<>();
 
-	private int capacity = -1; // default capacity - no limit
+	private int capacity = UNLIMITED;
 
 	public SimpleCache() {
 	}
@@ -20,7 +21,7 @@ class SimpleCache<K, V> {
 	}
 
 	public void put(K key, V value) {
-		if (capacity == -1 || cache.size() < capacity) {
+		if (capacity == UNLIMITED || cache.size() < capacity) {
 			cache.putIfAbsent(key, value);
 		}
 	}

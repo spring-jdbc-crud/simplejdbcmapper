b209148f8d7faa50c518caaf58647fa42a3d02df
ajoseph <ajosephmi@yahoo.com>
2026-03-21 12:22:05 -0400

    with synchronized block

--- src/main/java/io/github/simplejdbcmapper/core/TableMappingProvider.java
+++ src/main/java/io/github/simplejdbcmapper/core/TableMappingProvider.java
@@ -203,20 +203,22 @@ class TableMappingProvider {
 	}
 
 	private String getDatabaseProductName() {
-		// No side effects even if there is thread contention and it gets set more than
-		// once
 		if (databaseProductName == null) {
-			try {
-				databaseProductName = JdbcUtils.extractDatabaseMetaData(dataSource,
-						new DatabaseMetaDataCallback<String>() {
-							public String processMetaData(DatabaseMetaData dbMetaData)
-									throws SQLException, MetaDataAccessException {
-								return dbMetaData.getDatabaseProductName() == null ? ""
-										: dbMetaData.getDatabaseProductName();
-							}
-						});
-			} catch (Exception e) {
-				throw new MapperException(e);
+			synchronized (this) {
+				if (databaseProductName == null) {
+					try {
+						databaseProductName = JdbcUtils.extractDatabaseMetaData(dataSource,
+								new DatabaseMetaDataCallback<String>() {
+									public String processMetaData(DatabaseMetaData dbMetaData)
+											throws SQLException, MetaDataAccessException {
+										return dbMetaData.getDatabaseProductName() == null ? ""
+												: dbMetaData.getDatabaseProductName();
+									}
+								});
+					} catch (Exception e) {
+						throw new MapperException(e);
+					}
+				}
 			}
 		}
 		return databaseProductName;

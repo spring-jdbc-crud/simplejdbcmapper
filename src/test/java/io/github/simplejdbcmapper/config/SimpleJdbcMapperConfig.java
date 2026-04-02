f31fde3eb1f9022867c7ee8a0db610929556df57
ajoseph <ajosephmi@yahoo.com>
2026-04-02 10:06:09 -0400

    ci with oracle

--- src/test/java/io/github/simplejdbcmapper/config/SimpleJdbcMapperConfig.java
+++ src/test/java/io/github/simplejdbcmapper/config/SimpleJdbcMapperConfig.java
@@ -40,8 +40,6 @@ public class SimpleJdbcMapperConfig {
 		if (jdbcDriver.contains("mysql")) {
 			// schema1 database name for mysql
 			simpleJdbcMapper = new SimpleJdbcMapper(dataSource, null, "schema1");
-		} else if (jdbcDriver.contains("oracle")) {
-			simpleJdbcMapper = new SimpleJdbcMapper(dataSource, "SCHEMA1");
 		} else {
 			simpleJdbcMapper = new SimpleJdbcMapper(dataSource, "schema1");
 		}

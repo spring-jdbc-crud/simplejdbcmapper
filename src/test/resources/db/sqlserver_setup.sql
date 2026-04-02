da49164a8cc47b0f626106ee0fb35558ef363d0e
ajoseph <ajosephmi@yahoo.com>
2026-04-02 14:12:08 -0400

    ci with sqlserver

--- src/test/resources/db/sqlserver_setup.sql
+++ src/test/resources/db/sqlserver_setup.sql
@@ -1,8 +1,16 @@
+GO
 create schema schema1 AUTHORIZATION dbo;
+GO
 create schema schema2 AUTHORIZATION dbo;
+GO
 create user user_schema1 for login user_schema1;
+GO
 create user user_schema2 for login user_schema2;
+GO
 alter role db_owner add member user_schema1;
+GO
 alter role db_owner add member user_schema2;
+GO
 alter user user_schema1 with default_schema = schema1;
+GO
 alter user user_schema2 with default_schema = schema2;

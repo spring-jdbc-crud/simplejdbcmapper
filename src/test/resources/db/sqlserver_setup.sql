78238896662b195fedca52e0002c759756b21957
ajoseph <ajosephmi@yahoo.com>
2026-04-02 14:30:33 -0400

    ci with sqlserver

--- src/test/resources/db/sqlserver_setup.sql
+++ src/test/resources/db/sqlserver_setup.sql
@@ -3,14 +3,18 @@ create schema schema1 AUTHORIZATION dbo;
 GO
 create schema schema2 AUTHORIZATION dbo;
 GO
-create user user_schema1 for login user_schema1;
+CREATE LOGIN userSchema1 WITH PASSWORD = 'Mssql123';
 GO
-create user user_schema2 for login user_schema2;
+CREATE LOGIN userSchema2 WITH PASSWORD = 'Mssql123';
 GO
-alter role db_owner add member user_schema1;
+create user userSchema1 for login userSchema1;
 GO
-alter role db_owner add member user_schema2;
+create user userSchema2 for login userSchema2;
 GO
-alter user user_schema1 with default_schema = schema1;
+alter role db_owner add member userSchema1;
 GO
-alter user user_schema2 with default_schema = schema2;
+alter role db_owner add member user_Schema2;
+GO
+alter user userSchema1 with default_schema = schema1;
+GO
+alter user userSchema2 with default_schema = schema2;

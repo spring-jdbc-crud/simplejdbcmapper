f8fd03ee0fcd352863dbff030ee4560cbeb8c112
ajoseph <ajosephmi@yahoo.com>
2026-04-02 14:33:17 -0400

    ci with sqlserver

--- src/test/resources/db/sqlserver_setup.sql
+++ src/test/resources/db/sqlserver_setup.sql
@@ -13,7 +13,7 @@ create user userSchema2 for login userSchema2;
 GO
 alter role db_owner add member userSchema1;
 GO
-alter role db_owner add member user_Schema2;
+alter role db_owner add member userSchema2;
 GO
 alter user userSchema1 with default_schema = schema1;
 GO

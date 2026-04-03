63af87bbdaf198fb5c1d2edef9a3ded2b02212d3
ajoseph <ajosephmi@yahoo.com>
2026-04-02 22:01:18 -0400

    ci with oracle

--- src/test/resources/db/oracle_setup.sql
+++ src/test/resources/db/oracle_setup.sql
@@ -1,4 +1,5 @@
 create user SCHEMA2 identified by app;
 grant all privileges to SCHEMA2;
 grant SELECT ANY DICTIONARY to SCHEMA2;
+grant all privileges to SCHEMA1;
 grant SELECT ANY DICTIONARY to SCHEMA1;

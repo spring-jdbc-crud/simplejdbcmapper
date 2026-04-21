5d66e9522356de8a587929389ad86d6fa6275a2a
ajoseph <ajosephmi@yahoo.com>
2026-04-21 18:50:15 -0400

    javadoc

--- src/main/java/io/github/simplejdbcmapper/core/EntityRowMapper.java
+++ src/main/java/io/github/simplejdbcmapper/core/EntityRowMapper.java
@@ -23,7 +23,7 @@ import org.springframework.jdbc.support.JdbcUtils;
 import io.github.simplejdbcmapper.exception.MapperException;
 
 /**
- * A row mapper which has been optimized for performance.
+ * A row mapper for a mapped objects.
  * 
  * Use the method sjm.newEntityRowMapper({@code Class<?>}) to get an instance of
  * EntityRowMapper.

ddffda24ffa4444778929d37f8900332ae26a317
ajoseph <ajosephmi@yahoo.com>
2026-05-01 08:26:21 -0400

    javadoc

--- src/main/java/io/github/simplejdbcmapper/relationship/Relationship.java
+++ src/main/java/io/github/simplejdbcmapper/relationship/Relationship.java
@@ -2,6 +2,11 @@ package io.github.simplejdbcmapper.relationship;
 
 import java.util.List;
 
+/**
+ * The starting point for any relationship assignment
+ * 
+ * @param <T> the type of the main list
+ */
 public class Relationship<T> implements RelationshipSpec<T> {
 
 	private List<T> mainObjList;
@@ -10,14 +15,38 @@ public class Relationship<T> implements RelationshipSpec<T> {
 		this.mainObjList = mainObjList;
 	}
 
+	/**
+	 * Start of creating a relationship
+	 * 
+	 * @param <T>
+	 * @param mainObjList the main object list
+	 * @return A relationship
+	 */
 	public static <T> RelationshipSpec<T> mainList(List<T> mainObjList) {
 		return new Relationship<>(mainObjList);
 	}
 
+	/**
+	 * A toOne relationship
+	 * 
+	 * @param <T>            the type of the main object
+	 * @param <U>            the type of the related object
+	 * 
+	 * @param relatedObjList the list of related objects
+	 * 
+	 */
 	public <U> ToOneSpec<T, U> toOneList(List<U> relatedObjList) {
 		return ToOne.toOne(mainObjList, relatedObjList);
 	}
 
+	/**
+	 * A toMany relationship
+	 * 
+	 * @param <T>            the type of the main object
+	 * @param <U>            the type of the related object
+	 * 
+	 * @param relatedObjList the list of related objects
+	 */
 	public <U> ToManySpec<T, U> toManyList(List<U> relatedObjList) {
 		return ToMany.toMany(mainObjList, relatedObjList);
 	}
--- src/main/java/io/github/simplejdbcmapper/relationship/RelationshipSpec.java
+++ src/main/java/io/github/simplejdbcmapper/relationship/RelationshipSpec.java
@@ -2,10 +2,29 @@ package io.github.simplejdbcmapper.relationship;
 
 import java.util.List;
 
+/**
+ * A relationship specification for populating relationships
+ */
 public interface RelationshipSpec<T> {
 
+	/**
+	 * A toOne relationship
+	 * 
+	 * @param <T>            the type of the main object
+	 * @param <U>            the type of the related object
+	 * 
+	 * @param relatedObjList the list of related objects
+	 */
 	<U> ToOneSpec<T, U> toOneList(List<U> relatedObjList);
 
+	/**
+	 * A toMany relationship
+	 * 
+	 * @param <T>            the type of the main object
+	 * @param <U>            the type of the related object
+	 * 
+	 * @param relatedObjList the list of related objects
+	 */
 	<U> ToManySpec<T, U> toManyList(List<U> relatedObjList);
 
 }
